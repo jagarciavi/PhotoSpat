5d53c2234218193864f7f8a4f19cca63496b171f
José A. García <jagarciavi1992@gmail.com>
2015-07-13 21:37:06 +0200

    Version 1.0.2
    - Many improvements on loading pages.
    - New loading dialog.
    - Basic error handling.
    - Use of counters (demigrante por mi parte - Forocoches word).
    
    Signed-off-by: José A. García <jagarciavi1992@gmail.com>

--- app/src/main/java/es/jagarciavi/photospat/PhotoSpat.java
+++ app/src/main/java/es/jagarciavi/photospat/PhotoSpat.java
@@ -1,53 +1,97 @@
 package es.jagarciavi.photospat;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class PhotoSpat extends AppCompatActivity {
+
+    private WebView webview;
+    private ProgressDialog progressDialog;
+    private int counter = 0;
+    private int firstLoad = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_spat);
 
+        webview = (WebView) findViewById(R.id.webview);
 
-        //this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
-        setContentView(R.layout.activity_photo_spat );
-        final Activity MyActivity = this;
-        // Makes Progress bar Visible
-        //getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
+        final Activity activity = this;
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress)
-            {
-                //Make the bar disappear after URL is loaded, and changes string to Loading...
-                MyActivity.setTitle("Cargando...");
-                MyActivity.setProgress(progress * 100); //Make the bar disappear after URL is loaded
+        // Enable JavaScript and lets the browser go back
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.canGoBack();
 
-                // Return the app name after finish loading
-                if(progress == 100)
-                    MyActivity.setTitle(R.string.app_name);
-            }
-        });
-
-        myWebView.setWebViewClient(new WebViewClient() {
-            @Override
+        webview.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //counter = 0;
                 view.loadUrl(url);
-                return false;
+                return true;
             }
+
+            public void onLoadResource(WebView view, String url) {
+                // Check to see if there is a progress dialog
+                if (progressDialog == null) {
+                    // If no progress dialog, make one and set message
+                    progressDialog = new ProgressDialog(activity);
+                    progressDialog.setMessage("Cargando, por favor, espere...");
+                    progressDialog.show();
+
+                    // Hide the webview while loading
+                    webview.setEnabled(false);
+                }
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                // Page is done loading;
+                // hide the progress dialog and show the webview
+
+                Log.i(null, "The app is in the onPageFinished method");
+
+                String finalUrl = webview.getUrl().toString();
+
+                Log.i(null, "finalurl string is " + finalUrl);
+
+                if (finalUrl.equals("https://instagram.com/accounts/login/")) {
+                    if(firstLoad == 1) {
+                        Log.i(null, "The strings are equal, we'll set counter value to 4");
+                        counter = 4;
+                    } else {
+                        Log.i(null, "It's not the load of the login page...");
+                        firstLoad = 1;
+                    }
+                }
+
+                if (progressDialog.isShowing() && counter == 4) {
+                    Log.i(null, "On loop -> Dismiss progress dialog ");
+                    progressDialog.dismiss();
+                    //progressDialog = null;
+                    webview.setEnabled(true);
+                    counter = 0;
+                }
+                Log.i(null, "Counter value is " + counter);
+                counter++;
+            }
+
+            public void onReceivedError(WebView view, int errorCod,String description, String failingUrl) {
+                Toast.makeText(PhotoSpat.this, "Your Internet Connection May not be active Or " + description, Toast.LENGTH_LONG).show();
+            }
+
         });
-        myWebView.getSettings().setJavaScriptEnabled(true);
 
 
-        myWebView.loadUrl("http://www.instagram.com/accounts/login");
+        webview.loadUrl("https://www.instagram.com/accounts/login");
     }
 
     @Override
